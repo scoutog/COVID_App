51ba68a5c6e925f50f58791949cc15086fc6f289
Scout Oatman-Gaitan <54646126+scoutog@users.noreply.github.com>
2020-05-19 02:56:45 -0400

    Update covid_app.R

--- covid_app.R
+++ covid_app.R
@@ -1,6 +1,7 @@
 # See my app live at https://scout-oatman-gaitan.shinyapps.io/COVID_App/
 
 library(plotly)
+library(shiny)
 library(tidyverse)
 library(DT)
 library(shinythemes)
@@ -32,13 +33,22 @@ ui <- fluidPage(
       dateRangeInput("date", "Date Range:", start="2020-01-22",
                      end=(Sys.Date()-1)),
       selectInput("state", "State/Province:", unique(df$State), selected="New York"),
-      selectInput("county", "County:", unique(df$County)) ),
+      selectInput("county", "County:", unique(df$County)),
+      a("Data Source: CSSE at Johns Hopkins University (updated daily)", 
+        href="https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series")
+    ),
     mainPanel(
       tabsetPanel(
         tabPanel("Confirmed Count",
                  plotly::plotlyOutput("confirmed")),
         tabPanel("Death Count",
-                 plotly::plotlyOutput("deaths"))))
+                 plotly::plotlyOutput("deaths"))),
+      br(), br(),
+      p(em("* Click a county in the legend once to remove it and the graph will rescale")),
+      p(em("* Double click a county in the legend to focus on that county's data alone")),
+      br(), br(),
+      a("Check out my GitHub here", href="https://github.com/scoutog/COVID_App")
+    )
   )
 )
 server <- function(input, output, session) {
